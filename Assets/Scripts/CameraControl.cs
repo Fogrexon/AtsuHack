11e244b2ea9b0efeafa06e3b6a8ef142d37b3e66
Fogrexon <fogrexon@gmail.com>
2020-07-17 19:39:28 +0900

    wheel move

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     Camera camera;
+    Vector2 prevMouse;
 
     void Start() {
         camera = Camera.main;
@@ -18,5 +19,15 @@ public class CameraControl : MonoBehaviour
         if (d < 0f) {
             camera.orthographicSize = camera.orthographicSize * 1.1f;
         }
+
+        if (Input.GetMouseButton(2)) {
+            camera.transform.position = new Vector3(
+                -(Input.mousePosition.x - prevMouse.x) * camera.orthographicSize * 0.007f + camera.transform.position.x,
+                -(Input.mousePosition.y - prevMouse.y) * camera.orthographicSize * 0.007f + camera.transform.position.y,
+                camera.transform.position.z
+            );
+        }
+
+        prevMouse = Input.mousePosition;
     }
 }
