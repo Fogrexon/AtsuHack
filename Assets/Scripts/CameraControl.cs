9e7269456171254cd0fad917b006ebb18ee2034b
Fogrexon <fogrexon@gmail.com>
2020-07-18 00:04:13 +0900

    explain追加

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -22,8 +22,8 @@ public class CameraControl : MonoBehaviour
 
         if (Input.GetMouseButton(2) || Input.GetKey(KeyCode.Space)) {
             camera.transform.position = new Vector3(
-                -(Input.mousePosition.x - prevMouse.x) * camera.orthographicSize * 0.007f + camera.transform.position.x,
-                -(Input.mousePosition.y - prevMouse.y) * camera.orthographicSize * 0.007f + camera.transform.position.y,
+                -(Input.mousePosition.x - prevMouse.x) * camera.orthographicSize / Screen.height * 2f + camera.transform.position.x,
+                -(Input.mousePosition.y - prevMouse.y) * camera.orthographicSize / Screen.height * 2f + camera.transform.position.y,
                 camera.transform.position.z
             );
         }
